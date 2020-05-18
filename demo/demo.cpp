97bbaf0e6dcd618c06417383165bb702986b9256
Xiaozd <738140091@qq.com>
2020-05-18 16:01:48 +0800

    完成 点-模板

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,9 +1,14 @@
 #include "driver/lidar_driver.hpp"
-#include <msg/ros_msg_translator.h>
 #include <ros/ros.h>
 #include <ros/publisher.h>
-ros::Publisher lidar_points_pub_;
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <signal.h>
 
+ros::Publisher lidar_points_pub_;
+bool start_=true;
 struct PointXYZI
 {
     double x;
@@ -14,18 +19,41 @@ struct PointXYZI
 
 void callback(const robosense::LidarPointsMsg<PointXYZI> &msg)
 {
-    //lidar_points_pub_.publish(toRosMsg(msg));
-    DEBUG<<"msg: "<<msg.seq<<REND;
+
+    sensor_msgs::PointCloud2 ros_msg;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud2(new pcl::PointCloud<pcl::PointXYZI>);
+    for (auto iter : *msg.cloudPtr)
+    {
+        pcl::PointXYZI point;
+        point.x = iter.x;
+        point.y = iter.y;
+        point.z = iter.z;
+        point.intensity = iter.intensity;
+        cloud2->push_back(point);
+    }
+
+    pcl::toROSMsg(*cloud2, ros_msg);
+
+    ros_msg.header.stamp = ros_msg.header.stamp.fromSec(msg.timestamp);
+    ros_msg.header.frame_id = msg.parent_frame_id;
+    ros_msg.header.seq = msg.seq;
+
+    lidar_points_pub_.publish(ros_msg);
+
+    DEBUG << "msg: " << msg.seq << REND;
+}
+
+static void sigHandler(int sig)
+{
+    start_ = false;
 }
 
 int main(int argc, char *argv[])
 {
-    // ros::init(argc,argv,"driver");
-    // ros::NodeHandle nh_;
-    // lidar_points_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("points", 10);
-
-
-
+    signal(SIGINT, sigHandler); ///< bind the ctrl+c signal with the the handler function
+    ros::init(argc, argv, "driver", ros::init_options::NoSigintHandler);
+    ros::NodeHandle nh_;
+    lidar_points_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("points", 10);
     std::shared_ptr<robosense::sensor::LidarDriver<PointXYZI>> demo_ptr = std::make_shared<robosense::sensor::LidarDriver<PointXYZI>>();
     robosense::sensor::RSLiDAR_Driver_Param param;
     param.input_param.read_pcap = true;
@@ -35,7 +63,7 @@ int main(int argc, char *argv[])
     demo_ptr->init(param);
     demo_ptr->regRecvCallback(callback);
     demo_ptr->start();
-    while (true)
+    while (start_)
     {
         sleep(1);
     }
