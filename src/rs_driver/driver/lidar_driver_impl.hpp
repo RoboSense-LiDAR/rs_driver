0ec42d440f420a6ae9eb643b2464f38f84ecabed
Xiaozd <738140091@qq.com>
2020-11-11 17:08:24 +0800

    fix: fix the bug of not exiting

--- src/rs_driver/driver/lidar_driver_impl.hpp
+++ src/rs_driver/driver/lidar_driver_impl.hpp
@@ -451,6 +451,7 @@ inline void LidarDriverImpl<T_Point>::processMsop()
     else
     {
       reportError(Error(ERRCODE_WRONGPKTHEADER));
+      msop_pkt_queue_.clear();
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
   }
