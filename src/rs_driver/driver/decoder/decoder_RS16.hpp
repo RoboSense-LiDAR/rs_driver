a91f8bb5664b299ff0f9d4e9a47a2a2f88d67d5e
ron.zheng <ron.zheng@robosense.cn>
2022-01-11 16:41:14 +0800

    fix: pragma once

--- src/rs_driver/driver/decoder/decoder_RS16.hpp
+++ src/rs_driver/driver/decoder/decoder_RS16.hpp
@@ -30,6 +30,7 @@ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWIS
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *********************************************************************************************************************/
 
+#pragma once
 #include <rs_driver/driver/decoder/decoder_base.hpp>
 namespace robosense
 {
