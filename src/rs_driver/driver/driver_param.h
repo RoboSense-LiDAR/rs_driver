3eb904fcb52de93ce0da3e9c61c2078749760dc0
ron.zheng <ron.zheng@robosense.cn>
2022-03-25 11:43:24 +0800

    feat: print more driver_params

--- src/rs_driver/driver/driver_param.h
+++ src/rs_driver/driver/driver_param.h
@@ -122,6 +122,7 @@ typedef struct RSDecoderParam  ///< LiDAR decoder parameter
     RS_INFOL << "split_frame_mode: " << split_frame_mode << RS_REND;
     RS_INFOL << "num_pkts_split: " << num_pkts_split << RS_REND;
     RS_INFOL << "cut_angle: " << cut_angle << RS_REND;
+    RS_INFOL << "use_lidar_clock: " << use_lidar_clock << RS_REND;
     RS_INFO << "------------------------------------------------------" << RS_REND;
   }
 } RSDecoderParam;
@@ -146,6 +147,7 @@ typedef struct RSInputParam  ///< The LiDAR input parameter
     RS_INFO << "------------------------------------------------------" << RS_REND;
     RS_INFO << "             RoboSense Input Parameters " << RS_REND;
     RS_INFOL << "multi_cast_address: " << multi_cast_address << RS_REND;
+    RS_INFOL << "host_address: " << host_address << RS_REND;
     RS_INFOL << "msop_port: " << msop_port << RS_REND;
     RS_INFOL << "difop_port: " << difop_port << RS_REND;
     RS_INFOL << "read_pcap: " << read_pcap << RS_REND;
@@ -174,6 +176,7 @@ typedef struct RSDriverParam  ///< The LiDAR driver parameter
     RS_INFOL << "             RoboSense Driver Parameters " << RS_REND;
     RS_INFOL << "angle_path: " << angle_path << RS_REND;
     RS_INFOL << "frame_id: " << frame_id << RS_REND;
+    RS_INFOL << "wait_for_difop: " << wait_for_difop << RS_REND;
     RS_INFOL << "lidar_type: ";
     RS_INFO << lidarTypeToStr(lidar_type) << RS_REND;
     RS_INFOL << "------------------------------------------------------" << RS_REND;
