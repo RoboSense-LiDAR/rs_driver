4b3c38836e41165400d52c8ab0fcdd31dd981e1e
ron.zheng <ron.zheng@robosense.cn>
2022-07-01 17:53:44 +0800

    fix: fix pcap_rate

--- src/rs_driver/driver/input/input_pcap.hpp
+++ src/rs_driver/driver/input/input_pcap.hpp
@@ -51,7 +51,7 @@ class InputPcap : public Input
 public:
   InputPcap(const RSInputParam& input_param, double sec_to_delay)
     : Input(input_param), pcap_(NULL), pcap_offset_(ETH_HDR_LEN), pcap_tail_(0), difop_filter_valid_(false), 
-    msec_to_delay_((uint64_t)(sec_to_delay*1000000))
+    msec_to_delay_((uint64_t)(sec_to_delay / input_param.pcap_rate * 1000000))
   {
     if (input_param.use_vlan)
     {
--- src/rs_driver/driver/input/input_pcap_jumbo.hpp
+++ src/rs_driver/driver/input/input_pcap_jumbo.hpp
@@ -52,7 +52,7 @@ class InputPcapJumbo : public Input
 public:
   InputPcapJumbo(const RSInputParam& input_param, double sec_to_delay)
     : Input(input_param), pcap_(NULL), pcap_offset_(ETH_HDR_LEN), pcap_tail_(0), difop_filter_valid_(false), 
-    msec_to_delay_((uint64_t)(sec_to_delay*1000000))
+    msec_to_delay_((uint64_t)(sec_to_delay / input_param.pcap_rate * 1000000))
   {
     if (input_param.use_vlan)
     {
