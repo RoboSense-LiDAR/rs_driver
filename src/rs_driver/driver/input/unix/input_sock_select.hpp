d294e3cb0c8982c0968bd5abf9c9107e56678353
ron.zheng <ron.zheng@robosense.cn>
2022-07-19 16:28:49 +0800

    feat: add option to double RCVBUF

--- src/rs_driver/driver/input/unix/input_sock_select.hpp
+++ src/rs_driver/driver/input/unix/input_sock_select.hpp
@@ -188,6 +188,16 @@ inline int InputSock::createSocket(uint16_t port, const std::string& hostIp, con
     }
   }
 
+#ifdef ENABLE_DOUBLE_RCVBUF
+  {
+    uint32_t opt_val;
+    socklen_t opt_len = sizeof(uint32_t);
+    getsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char*)&opt_val, &opt_len);
+    opt_val *= 4;
+    setsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char*)&opt_val, opt_len);
+  }
+#endif
+
   {
     int flags = fcntl(fd, F_GETFL, 0);
     ret = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
