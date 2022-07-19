cb36710cf66b067d0e93a342db2c9e3586316557
ron.zheng <ron.zheng@robosense.cn>
2022-07-19 15:59:27 +0800

    fix: fix runtime error in case of ENABLE_TRANSFORM

--- src/rs_driver/driver/decoder/decoder.hpp
+++ src/rs_driver/driver/decoder/decoder.hpp
@@ -248,6 +248,10 @@ class Decoder
 {
 public:
 
+#ifdef ENABLE_TRANSFORM
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+#endif
+
   constexpr static uint16_t PROTOCOL_VER_0 = 0x00;
 
   virtual void decodeDifopPkt(const uint8_t* pkt, size_t size) = 0;
