3f0620a6d0197f02db2fff07a08522583522f900
Zining Li <zining.li@intel.com>
2026-05-21 10:32:38 +0800

    fix: add missing <cstdint> include in driver_param.hpp and input.hpp

--- src/rs_driver/driver/driver_param.hpp
+++ src/rs_driver/driver/driver_param.hpp
@@ -38,6 +38,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <map>
 #include <cstring>
 #include <unordered_map>
+#include <cstdint>
 namespace robosense
 {
 namespace lidar
